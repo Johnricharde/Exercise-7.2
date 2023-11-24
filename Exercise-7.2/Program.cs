6f7e13b0050f0f18fcd0c31245e05fbdc918ff67
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-11-24 14:12:48 +0100

    Add project files.

---
+++ Exercise-7.2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercise_7._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                Console.WriteLine($"Loop cycle: {i}");
+                if (i == 5000)
+                {
+                    Console.WriteLine(args[999]);
+                }
+            }
+        }
+    }
+}
